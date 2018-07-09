4181dd1561d8be0007b38a2dc3d44b8399042c6e
Mohammed Shoaib <32389312+shoaib98libra@users.noreply.github.com>
2018-07-09 15:19:52 +0400

    Add files via upload

--- Player1.js
+++ Player1.js
@@ -3,8 +3,8 @@ function Player1(x,y)
 	this.x;
 	this.y;
 	this.show = function(){
-		line(this.x-30,this.y-30,this.x+30,this.y+30);
-		line(this.x-30,this.y+30,this.x+30,this.y-30);
+		line(this.x-(size/16.67),this.y-(size/16.67),this.x+(size/16.67),this.y+(size/16.67));
+		line(this.x-(size/16.67),this.y+(size/16.67),this.x+(size/16.67),this.y-(size/16.67));
 	}
 
 	this.findPos = function(mouseX,mouseY){
@@ -12,11 +12,11 @@ function Player1(x,y)
 		{
 			for(let j=1 ; j <= 3 ; j++)
 			{
-				if(mouseY >= 100*i && mouseY <= 100*(i+1))
-					if(mouseX >= 100*j && mouseX <= 100*(j+1))
+				if(mouseY >= (size/5)*i && mouseY <= (size/5)*(i+1))
+					if(mouseX >= (size/5)*j && mouseX <= (size/5)*(j+1))
 					{
-						this.y = 100*i + 50;
-						this.x = 100*j + 50;
+						this.y = (size/5)*i + size/10;
+						this.x = (size/5)*j + size/10;
 					}
 			}
 		}
--- Sketch.js
+++ Sketch.js
@@ -1,12 +1,7 @@
-let cnv;
-let playerChance;
-let player1;
-let player2 ;
-let player1Win, player2Win,playerTie;
-let player1Score,player2Score;
-let pauseScreen;
-let game;
-let startTime;
+let cnv,size;
+let playerChance,player1,player2 ;
+let player1Win, player2Win,playerTie,endGame,player1Score,player2Score;
+let pauseScreen,gamesPlayed,startTime;
 
 function centerCanvas()
 {
@@ -17,11 +12,20 @@ function centerCanvas()
 
 function windowResized()
 {
-	centerCanvas();
+	setup();
 }
 
 function setup()
 {
+	//Creating the canvas based on window size
+	if(windowWidth > windowHeight)
+		size = windowHeight;
+	else
+		size = windowWidth;
+	cnv = createCanvas(size,size);
+	centerCanvas();
+
+	//Default values for all the variables
 	playerChance = 1;
 	player1 = [];
 	player2 = [];
@@ -29,27 +33,31 @@ function setup()
 	player2Win = false;
 	playerTie = false;
 	pauseScreen = false;
+	endGame = false;
 	player1Score = 0;
 	player2Score = 0;
-	game = 1;
-	cnv = createCanvas(500,500);
-	centerCanvas();
+	gamesPlayed = 1;
+	startTime = 0;
 }
 	
 function nextGame()
 {
+	//Next game values for all the variables
 	player1 = [];
 	player2 = [];
 	player1Win = false;
 	player2Win = false;
 	playerTie = false;
 	pauseScreen = false;
-	game++;
-	if(game %2 === 0)
+	endGame = false;
+	startTime = 0;
+	gamesPlayed++;
+
+	//Checking which player starts
+	if(gamesPlayed%2 === 0)
 		playerChance = 2;
 	else 
 		playerChance = 1;
-	createCanvas(500,500);
 }
 
 function draw()
@@ -58,65 +66,36 @@ function draw()
 	{
 		background(51);
 		stroke(255);
-		strokeWeight(5);
-		line(200,100,200,400);
-		line(300,100,300,400);
-		line(100,200,400,200);
-		line(100,300,400,300);
+		strokeWeight(size/100);
+		line(size/2.50,size/5.00,size/2.50,size/1.25);
+		line(size/1.67,size/5.00,size/1.67,size/1.25);
+		line(size/5.00,size/2.50,size/1.25,size/2.50);
+		line(size/5.00,size/1.67,size/1.25,size/1.67);
 	}
 	if(pauseScreen && Date.now() - startTime < 2000)
 	{
 		//Do nothing just pause the screen
 	}
-	else if(player1Win && (Date.now() - startTime >= 2000) )
+	else if(endGame && Date.now() - startTime >= 2000)
 	{
 		push();
 		stroke(255);
 		strokeWeight(1);
 		fill(255);
 		background(51);
-		textSize(32);
-		text("Player 1 Wins!",140,100);
-		text("Score now:",170,160);
-		text("Player 1: " + player1Score,10,220);
-		text("Player 2: " + player2Score,330,220);
-		text("Games Played: " + game,120,280);
-		text("Tap or Press SpaceBar",90,350);
-		text("to go to the next Game!",90,400);
-		pop();
-	}
-	else if(player2Win  && (Date.now() - startTime >= 2000) )
-	{
-		push();
-		stroke(255);
-		strokeWeight(1);
-		fill(255);
-		background(51);
-		textSize(32);
-		text("Player 2 Wins!",140,100);
-		text("Score now:",170,160);
-		text("Player 1: " + player1Score,10,220);
-		text("Player 2: " + player2Score,330,220);
-		text("Games Played: " + game,120,280);
-		text("Tap or Press SpaceBar",90,350);
-		text("to go to the next Game!",90,400);
-		pop();
-	}
-	else if(playerTie  && (Date.now() - startTime >= 2000) )
-	{
-		push();
-		stroke(255);
-		strokeWeight(1);
-		fill(255);
-		background(51);
-		textSize(32);
-		text("It's a Tie!",190,100);
-		text("Score now:",170,160);
-		text("Player 1: " + player1Score,10,220);
-		text("Player 2: " + player2Score,330,220);
-		text("Games Played: " + game,120,280);
-		text("Tap or Press SpaceBar",90,350);
-		text("to go to the next Game!",90,400);
+		textSize(size/15.625);
+		if(player1Win)
+			text("Player 1 Wins!",size/3.57,size/5);
+		else if(player2Win)
+			text("Player 2 Wins!",size/3.57,size/5);
+		else if(playerTie)
+			text("It's a Tie!",size/2.63,size/5);
+		text("Score now:",size/2.94,size/3.125);
+		text("Player 1: " + player1Score,size/50,size/2.27);
+		text("Player 2: " + player2Score,size/1.52,size/2.27);
+		text("Games Played: " + gamesPlayed,size/4.17,size/1.79);
+		text("Tap or Press SpaceBar",size/5.56,size/1.43);
+		text("to go to the next Game!",size/5.56,size/1.25);
 		pop();
 	}
 	else
@@ -127,18 +106,20 @@ function draw()
 		player2[i].show();
 	checkWin(player1,player2);
 	checkIfTie();
+
+	//Displaying all the text
 	push();
 	stroke(255);
 	strokeWeight(1);
 	fill(255);
-	textSize(25);
-	text("Player Chance: " + playerChance,160,480);
-	text("Player 1",10,40);
-	text("Player 2",380,40);
-	text("Game",220,40);
-	text(game,245,80);
-	text(player1Score,40,80);
-	text(player2Score,420,80);
+	textSize(size/20);
+	text("Player Chance: " + playerChance,size/3.125,size/1.11);
+	text("Player 1",size/50,size/12.5);
+	text("Player 2",size/1.32,size/12.5);
+	text("Game",size/2.27,size/12.5);
+	text(gamesPlayed,size/2.04,size/6.25);
+	text(player1Score,size/12.5,size/6.25);
+	text(player2Score,size/1.19,size/6.25);
 	pop();
 	}//End of else
 }
@@ -147,9 +128,9 @@ function mousePressed()
 {
 	if(player1Win || player2Win || playerTie)
 		nextGame();
-	else if(mouseX >= 100 && mouseX <= 400)
+	else if(mouseX >= size/5 && mouseX <= size/1.25)
 	{
-		if(mouseY >= 100 && mouseY <= 400)
+		if(mouseY >= size/5 && mouseY <= size/1.25)
 		{
 			if(!checkIfPlayerExists(player1,player2,mouseX,mouseY))
 			{
@@ -179,17 +160,19 @@ function keyPressed()
 
 function checkIfPlayerExists(player1,player2,X,Y)
 {
-	let i = floor(Y/100);
-	let j = floor(X/100);
+	//Returns true if player already exists at a given position
+
+	let i = floor(Y/(size/5));
+	let j = floor(X/(size/5));
 
 	for(let k=0 ; k < player1.length ; k++)
 	{
-		if( ( (j*100 + 50) === player1[k].x) && ( ( i*100 + 50) === player1[k].y) )
+		if( ( (j*(size/5) + size/10) === player1[k].x) && ( ( i*(size/5) + size/10) === player1[k].y) )
 			return true;
 	}
 	for(let k=0 ; k < player2.length ; k++)
 	{
-		if( ( (j*100 + 50) === player2[k].x) && ( ( i*100 + 50) === player2[k].y) )
+		if( ( (j*(size/5) + size/10) === player2[k].x) && ( ( i*(size/5) + size/10) === player2[k].y) )
 			return true;
 	}
 	return false;
@@ -197,23 +180,27 @@ function checkIfPlayerExists(player1,player2,X,Y)
 
 function checkWin(player1,player2)
 {
+	//Checks if a player won the game
+
 	startTime = Date.now();
 	let count = 0;
 	stroke("red");
+
 	//Checking for all horizontal lines for player1
 	for(let i=1 ; i <= 3 ; i++)
 	{
 		for(let j=1 ; j <= 3 ; j++)
 			for(let k=0 ; k < player1.length ; k++)
-				if( ( (100*j + 50 ) === player1[k].x ) && ( (100*i + 50 ) === player1[k].y) )
+				if( ( ( (size/5)*j + size/10 ) === player1[k].x ) && ( ( (size/5)*i + size/10 ) === player1[k].y) )
 					count++;
 
 		if(count === 3)
 		{
-			line(100 , 100*i + 50 , 400 , 100*i + 50);
+			line( size/5 , (size/5)*i + size/10 , size/1.25 , (size/5)*i + size/10 );
 			player1Score++;
 			player1Win = true;
 			pauseScreen = true;
+			endGame = true;
 			return;
 		}
 			else
@@ -225,15 +212,16 @@ function checkWin(player1,player2)
 	{
 		for(let j=1 ; j <= 3 ; j++)
 			for(let k=0 ; k < player2.length ; k++)
-				if( ( (100*j + 50 ) === player2[k].x ) && ( (100*i + 50 ) === player2[k].y) )
+				if( ( ( (size/5)*j + size/10 ) === player2[k].x ) && ( ( (size/5)*i + size/10 ) === player2[k].y) )
 					count++;
 
 		if(count === 3)
 		{
-			line(100 , 100*i + 50 , 400 , 100*i + 50);
+			line(size/5 , (size/5)*i + size/10 , size/1.25 , (size/5)*i + size/10);
 			player2Score++;
 			player2Win = true;
 			pauseScreen = true;
+			endGame = true;
 			return;
 		}
 			else
@@ -245,15 +233,16 @@ function checkWin(player1,player2)
 	{
 		for(let i=1 ; i <= 3 ; i++)
 			for(let k=0 ; k < player1.length ; k++)
-				if( ( (100*j + 50) === player1[k].x) && ( (100*i + 50) === player1[k].y ) )
+				if( ( ((size/5)*j + size/10) === player1[k].x) && ( ( (size/5)*i + size/10) === player1[k].y ) )
 					count++;
 
 		if(count === 3)
 		{
-			line(100*j + 50 , 100 , 100*j + 50 , 400);
+			line((size/5)*j + size/10 , size/5 , (size/5)*j + size/10 , size/1.25);
 			player1Score++;
 			player1Win = true;
 			pauseScreen = true;
+			endGame = true;
 			return;
 		}
 			else
@@ -265,15 +254,16 @@ function checkWin(player1,player2)
 	{
 		for(let i=1 ; i <= 3 ; i++)
 			for(let k=0 ; k < player2.length ; k++)
-				if( ( (100*j + 50) === player2[k].x) && ( (100*i + 50) === player2[k].y ) )
+				if( ( ( (size/5)*j + size/10) === player2[k].x) && ( ( (size/5)*i + size/10) === player2[k].y ) )
 					count++;
 				
 		if(count === 3)
 		{
-			line(100*j + 50 , 100 , 100*j + 50 , 400);
+			line((size/5)*j + size/10 , (size/5) , (size/5)*j + size/10 , size/1.25);
 			player2Score++;
 			player2Win = true;
 			pauseScreen = true;
+			endGame = true;
 			return;
 		}
 			else
@@ -284,16 +274,17 @@ function checkWin(player1,player2)
 	for(let i=1,j=1 ; i <= 3 ; i++,j++)
 	{
 		for(let k=0 ; k < player1.length ; k++)
-			if( ( (100*j + 50) === player1[k].x) && ( (100*i + 50) === player1[k].y ) )
+			if( ( ( (size/5)*j + size/10) === player1[k].x) && ( ( (size/5)*i + size/10) === player1[k].y ) )
 				count++;
 	}
 
 	if(count === 3)
 	{
-		line(100 , 100 , 400 , 400);
+		line(size/5 , size/5 , size/1.25 , size/1.25);
 		player1Score++;
 		player1Win = true;
 		pauseScreen = true;
+		endGame = true;
 		return;
 	}
 	else
@@ -302,15 +293,16 @@ function checkWin(player1,player2)
 	//Checking for top left diagonal to bottom right for player2
 	for(let i=1,j=1 ; i <= 3 ; i++,j++)
 		for(let k=0 ; k < player2.length ; k++)
-			if( ( (100*j + 50) === player2[k].x) && ( (100*i + 50) === player2[k].y ) )
+			if( ( ( (size/5)*j + size/10) === player2[k].x) && ( ( (size/5)*i + size/10) === player2[k].y ) )
 				count++;
 
 	if(count === 3)
 	{
-		line(100 , 100 , 400 , 400);
+		line(size/5 , size/5 , size/1.25 , size/1.25);
 		player2Score++;
 		player2Win = true;
 		pauseScreen = true;
+		endGame = true;
 		return;
 	}
 	else
@@ -319,15 +311,16 @@ function checkWin(player1,player2)
 	//Checking for top right diagonal to bottom left for player1
 	for(let i=1,j=3 ; i <= 3 ; i++,j--)
 		for(let k=0 ; k < player1.length ; k++)
-			if( ( (100*j + 50) === player1[k].x ) && ( (100*i + 50) === player1[k].y ) )
+			if( ( ( (size/5)*j + size/10) === player1[k].x ) && ( ( (size/5)*i + size/10) === player1[k].y ) )
 				count++;
 
 	if(count === 3)
 	{
-		line(400 , 100 , 100 , 400);
+		line(size/1.25 , size/5 , size/5 , size/1.25);
 		player1Score++;
 		player1Win = true;
 		pauseScreen = true;
+		endGame = true;
 		return;
 	}
 	else
@@ -336,15 +329,16 @@ function checkWin(player1,player2)
 	//Checking for top right diagonal to bottom left for player2
 	for(let i=1,j=3 ; i <= 3 ; i++,j--)
 		for(let k=0 ; k < player2.length ; k++)
-			if( ( (100*j + 50) === player2[k].x ) && ( (100*i + 50) === player2[k].y ) )
+			if( ( ( (size/5)*j + size/10) === player2[k].x ) && ( ( (size/5)*i + size/10) === player2[k].y ) )
 				count++;
 
 	if(count === 3)
 	{
-		line(400 , 100 , 100 , 400);
+		line(size/1.25 , size/5 , size/5 , size/1.25);
 		player2Score++;
 		player2Win = true;
 		pauseScreen = true;
+		endGame = true;
 		return;
 	}
 	else
@@ -353,11 +347,12 @@ function checkWin(player1,player2)
 
 function checkIfTie()
 {
+	//Returns true if it's a tie
 	if(player1.length + player2.length === 9)				//9 being the number of boxes or spaces
 		if(player1Win === false && player2Win === false)
 		{
-
 			playerTie = true;
 			pauseScreen = true;
+			endGame = true;
 		}
 }
