8384ee99acea6a1216baf99d898410687ebce806
Mohammed Shoaib <32389312+shoaib98libra@users.noreply.github.com>
2018-07-09 11:11:53 +0400

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,4 +1,5 @@
-let cnv,score,bird,pipe,pipeDistance;
+let cnv,sizeW,sizeH;
+let bird,pipe,pipeDistance,bg,count;
 let isDied,isFirstLaunch,pauseScreen,startTime;
 
 function centerCanvas()
@@ -10,110 +11,125 @@ function centerCanvas()
 
 function windowResized()
 {
-	centerCanvas();
+	//Anytime the Window is resized, create a new canvas with those values
+	setup();
+}
+
+function preload(){
+	//Loading the background
+	bg = loadImage("https://i.imgur.com/JhllwsV.png");
 }
 
 function setup()
 {
-	cnv = createCanvas(800,600);
-	centerCanvas();
-	bird = new Bird();
-	score = new Score();
-	pipe = [];
-	pipeDistance = 300;
-	for(let i=0 ; i<3 ; i++)
-	{
-		pipe.push( new Pipe() );
-		pipe[i].x = pipeDistance*(i+3);
+	//Calculating the size of the canvas
+	if(windowWidth >= windowHeight*1.5){
+		sizeH = windowHeight;
+		sizeW = Math.ceil(sizeH * 1.5);
 	}
+	else{
+		sizeW = Math.ceil(windowWidth);
+		sizeH = sizeW/1.5;
+	}
+	if(sizeW%2 !== 0)
+		sizeW += 1;
+	cnv = createCanvas(sizeW,sizeH);
+	centerCanvas();
+
+	pipe = [];
 	startTime = 0;
+	count = 0;
 	isDied = false;
 	isFirstLaunch = true;
 	pauseScreen = false;
+
+	//Creating the objects
+	pipeDistance = Math.ceil(sizeH/2.2)%2 ? Math.ceil(sizeH/2.2)+1 : Math.ceil(sizeH/2.2);
+	pipe.push(new Pipe(sizeW+pipeDistance));
+	bird = new Bird();
 }
 
 function resetSketch(){
 	pipe = [];
-	pipeDistance = 300;
-	bird = new Bird();
-	score = new Score();
-	for(let i=0 ; i<3 ; i++)
-	{
-		pipe.push( new Pipe() );
-		pipe[i].x = pipeDistance*(i+3);
-	}
 	startTime = 0;
+	count = 0;
 	isDied = false;
 	isFirstLaunch = false;
 	pauseScreen = false;
+
+	//Creating the objects
+	pipeDistance = Math.ceil(sizeH/2.2)%2 ? Math.ceil(sizeH/2.2)+1 : Math.ceil(sizeH/2.2);
+	pipe.push(new Pipe(sizeW+pipeDistance));
+	bird = new Bird();
 }
 
 function draw()
 {
 	if(isFirstLaunch)
 	{
-		background(51);
-		stroke(255);
-		fill(255);
-		textSize(32);
-		text("Welcome to Flappy Bird!",200,275);
-		text("Tap or Press Spacebar to play!",165,325);
+		background(255);
+		fill(0);
+		stroke(0);
+		textSize(sizeH/15);
+		text("Welcome to Flappy Bird!",sizeH/2.4,sizeH/2.2);
+		text("Tap or Press Spacebar to play!",sizeH/3,sizeH/1.75);
 	}else if(pauseScreen && Date.now() - startTime < 2000)
 		;//Do nothing, just pause the screen yo)
 	else if(pauseScreen && Date.now() - startTime >= 2000)
 		pauseScreen = false;
 	else if(isDied){
-		background(51);
-		stroke(255);
-		fill(255)
-		textSize(32);
-		text("You Died!",300,250);
-		text("Score: " + score.count,305,300);
-		text("Tap or Press Spacebar to play!",165,350);
+		background(255);
+		stroke(0);
+		fill(0)
+		textSize(sizeH/15);
+		text("You Died!",sizeH/1.65,sizeH/2.2);
+		text("Score: " + count,sizeH/1.6,sizeH/1.76);
+		text("Tap or Press Spacebar to play!",sizeH/3.1,sizeH/1.47);
 	}else{
-	background(51);
+	background(255);	//Not required as there is a background in the assets
 	noStroke();
-	for(let i=0 ; i<3 ; i++)
-		pipe[i].show();
+
+	//Displaying the bird
+	image(bg,0,0,sizeW,sizeH);
 	bird.show();
 	bird.update();
-	fill(255);
-	score.show();
-	for(let i=0 ; i<pipe.length ; i++)
+
+	//Pushing a new pipe if the last pipe crosses a certain limit
+	let index = pipe.length-1;
+	if(pipe[index].x < sizeW)
+		pipe.push(new Pipe(sizeW+pipeDistance));
+
+	//Checking for pipes
+	for(let i=pipe.length-1 ; i>=0 ; i--){
+		pipe[i].show();
+
+		//Incrementing the score if the bird crosses a pipe
+		if(Math.ceil(bird.x) === Math.ceil(pipe[i].x) && !pipe[i].isFinished){
+			count++;
+			pipe[i].isFinished = true;
+		}
+		//Checking if the bird hit the pipe
 		if(bird.hit(pipe[i])){
 			startTime = Date.now();
 			isDied = true;
 			pauseScreen = true;
 		}
-	score.checkScore();
-	fill("black");
-	textSize(50);
-	text(score.count,checkDigits(),50);
+
+		//Splicing the pipe if it crosses the screen
+		if(pipe[i].x + pipe[i].width < 0)
+			pipe.splice(i,1);
 	}
 
-}
-
-
-function Score()
-{
-	this.count = 0;
-	this.checkScore = function(){
-		for(let i=0 ; i<3 ; i++)
-		{
-			if(bird.x === pipe[i].x)
-			{
-				if(bird.y > height-pipe[i].height-150 && bird.y < height-pipe[i].height)
-					this.count++;
-			}
-		}
-	}
-	this.show = function(){
-		rect(370,0,70,60);
+	//Displaying the score
+	fill(255);
+	textSize(sizeH/12.5);
+	text(count,checkDigits(),sizeH/10);
 	}
 }
 
 function mousePressed(){
-	if( (mouseX >= 0 && mouseX <= width) && (mouseY >= 0 && mouseY <= height) ){
+	//Checking if the mouse is pressed within the canvas
+	if( (mouseX >= 0 && mouseX <= sizeW) && (mouseY >= 0 && mouseY <= sizeH) ){
 		if(isFirstLaunch)
 			isFirstLaunch = false;
 		else if(!pauseScreen && isDied)
@@ -135,8 +151,8 @@ function keyPressed()
 
 function checkDigits()
 {
-	if(score.count/10 < 1)
-		return 390;
+	if(count/10 < 1)
+		return sizeW/2;
 	else
-		return 375;
+		return sizeW/2.05;
 }
--- Pipe.js
+++ Pipe.js
@@ -1,22 +1,18 @@
-function Pipe()
+function Pipe(x)
 {
-	this.height = random(400) + 50;		//Height of a pipe
-	this.width = 100;					//Width of a pipe
-	this.gap = 150;						//Gap between 2 pipes
-	this.x  = width  - this.width/2;						//Making the pipes start from the edge of the screen
-	this.y1 = height - this.height;
-	this.y2 = height - this.height-this.gap;
+	this.width = Math.ceil(height/5.5);											//Width of a pipe
+	this.gap = height/3.78;														//Gap between 2 pipes
+	this.height = random(sizeH/100,height/1.089 - this.gap - height/6.62);		//Height of a pipe
+	this.x  = x;																//Making the pipes start from the edge of the screen
+	this.y2 = this.height;
+	this.y1 = this.height + this.gap;
+	this.decreaseX = sizeH/331;													//The value of x that gets decremented each time
+	this.isFinished = false;													//To avoid increasing the score more than once for the same pipe
+
 	this.show = function(){
 		fill("green");
-		rect(this.x,this.y1,this.width,this.height);
-		rect(this.x,this.y2,this.width,-(height-this.height));
-		this.x -= 2;
-		if(this.x < -80)
-		{
-			this.x = width;
-			this.height = random(400) + 50;
-			this.y1 = height-this.height;
-			this.y2 = height-this.height-this.gap;
-		}
+		rect(this.x,this.y1,this.width,height/1.087 - this.y1);
+		rect(this.x,this.y2,this.width,-this.height);
+		this.x -= this.decreaseX;
 	}
 }
