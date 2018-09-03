7351853b4eb9237f1e46c2859220b4b993c067a5
Mohammed Shoaib <32389312+Mohammed-Shoaib@users.noreply.github.com>
2018-09-03 23:09:07 +0400

    Add files via upload

---
+++ data_to_pickle.py
@@ -0,0 +1,143 @@
+import os
+import json
+import pickle
+import numpy as np
+import keras
+from keras.applications import MobileNet
+
+print("Done importing")
+NUM_CLASSES = 2
+mapping = {'A': 0, 'Y': 1}
+
+
+def load_mobilenet():
+    """Loads the MobileNet model"""
+    print("Loading the MobileNet model...")
+    mobilenet = MobileNet(weights='imagenet')
+    print("Model Loaded.")
+    layer = mobilenet.get_layer('conv_pw_13_relu')
+    return keras.Model(inputs=mobilenet.inputs, outputs=layer.output)
+
+
+def load_json_file(file_path):
+    """Loads the data file"""
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def load_json_s():
+    s = '''
+    {
+        "entries": []
+    }
+    '''
+    data = json.loads(s)
+    return data
+
+
+def preprocess_data(data):
+    """Pre-Processes the data"""
+    print("Pre-processing the data...")
+    data = np.array(data['entries'])
+    # Shuffling the data
+    np.random.shuffle(data)
+    for i in range(len(data)):
+        # Converting from GRAYSCALE to RGB by duplication of values
+        for j in range(len(data[i]['pixels'])):
+            pixel = data[i]['pixels'][j]
+            data[i]['pixels'][j] = np.full(3, pixel)
+        # Shaping the pixels to a 224×224×3 numpy array
+        data[i]['pixels'] = np.array(data[i]['pixels'])
+        data[i]['pixels'] = np.reshape(data[i]['pixels'], (224, 224, 3))
+        # Converting letters to numbers from 0-26
+        data[i]['label'] = mapping[data[i]['label']]
+        print("Finished Pre-processing {} of {}".format((i+1), len(data)))
+    print("Data pre-processed.")
+    return data
+
+
+def load_data():
+    data = load_json_s()
+    for label in labels:
+        label_dir = data_path + label + '/'
+        label_1000_dir = os.listdir(label_dir)
+        for label_1000 in label_1000_dir:
+            file_path = label_dir + label_1000
+            print("Loading " + label_1000)
+            data_1000 = load_json_file(file_path)
+            data['entries'].extend(data_1000['entries'])
+    return data
+
+
+def split_data(data):
+    """Gets the data and split it into training and test sets"""
+    print("Creating the training and test sets...")
+    data_pixels = []
+    data_labels = []
+    for entry in data:
+        data_pixels.append(entry['pixels'])
+        data_labels.append(entry['label'])
+
+    # Splitting the data
+    offset = int(len(data)*0.7)
+    train_xs = np.array(data_pixels[:offset])
+    train_ys = np.array(data_labels[:offset])
+    test_xs = np.array(data_pixels[offset:])
+    test_ys = np.array(data_labels[offset:])
+
+    # One-Hot encoding for the labels
+    train_ys = keras.utils.to_categorical(train_ys, NUM_CLASSES)
+    test_ys = keras.utils.to_categorical(test_ys, NUM_CLASSES)
+    print("Data created.")
+    return [(train_xs, train_ys), (test_xs, test_ys)]
+
+
+def create_pickle(data, pickle_path):
+    """Creates pickle object for a given data"""
+    pickle_out = open(pickle_path, 'wb')
+    pickle.dump(data, pickle_out)
+    pickle_out.close()
+
+
+def get_activations(xs):
+    """Get the activations from internal layer for a set of images using mobilenet"""
+    print("Getting activations")
+    xs_activations = []
+    for i in range(len(xs)):
+        x = np.expand_dims(xs[i], axis=0)
+        x = mobilenet.predict(x)
+        x = np.squeeze(x, axis=0)
+        xs_activations.append(x)
+        print("Finished activation {} of {}".format((i+1), len(xs)))
+    xs_activations = np.array(xs_activations)
+    return xs_activations
+
+
+mobilenet = load_mobilenet()
+data_path = 'data/JSON Files/'
+pickle_path = 'data/Pickle Objects/'
+labels = os.listdir(data_path)
+data = load_data()
+data = preprocess_data(data)
+(train_xs, train_ys), (test_xs, test_ys) = split_data(data)
+train_activations = get_activations(train_xs)
+test_activations = get_activations(test_xs)
+
+# Creating the pickle objects for everything
+print("Creating Pickle Objects...")
+file_path = pickle_path + 'data.pickle'
+create_pickle(data, file_path)
+file_path = pickle_path + 'train_xs.pickle'
+create_pickle(train_xs, file_path)
+file_path = pickle_path + 'train_ys.pickle'
+create_pickle(train_ys, file_path)
+file_path = pickle_path + 'test_xs.pickle'
+create_pickle(test_xs, file_path)
+file_path = pickle_path + 'test_ys.pickle'
+create_pickle(test_ys, file_path)
+file_path = pickle_path + 'train_activations.pickle'
+create_pickle(train_activations, file_path)
+file_path = pickle_path + 'test_activations.pickle'
+create_pickle(test_activations, file_path)
+print("Created Objects.")
--- keras_model.py
+++ keras_model.py
@@ -1,50 +1,190 @@
 import json
+import pickle
+import numpy as np
 import keras
 from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.applications import MobileNet
+import tensorflowjs as tfjs
 
 print("Done importing")
 
 NUM_CLASSES = 2
+BATCH_SIZE = 64
+labels = {'A': 0, 'Y': 1}
 
 
-# Function to get the data and split it into training and test sets
-def create_data():
-    print("Loading the data...")
-    file_path = 'data/data.json'
+def load_mobilenet():
+    """Loads the MobileNet model"""
+    print("Loading the MobileNet model...")
+    mobilenet = MobileNet(weights='imagenet')
+    print("Model Loaded.")
+    layer = mobilenet.get_layer('conv_pw_13_relu')
+    return keras.Model(inputs=mobilenet.inputs, outputs=layer.output)
+
+
+def load_data(file_path):
+    """Loads the data file"""
+    print("Loading the json data...")
     with open(file_path, 'r') as f:
         data = json.load(f)
+    data = preprocess_data(data)
     print("Data Loaded.")
+    return data
 
 
-# Function to create the model
+def preprocess_data(data):
+    """Pre-Processes the data"""
+    print("Pre-processing the data...")
+    data = np.array(data['entries'])
+    # Shuffling the data
+    np.random.shuffle(data)
+    for entry in data:
+        # Converting from GRAYSCALE to RGB by duplication of values
+        for i in range(len(entry['pixels'])):
+            pixel = entry['pixels'][i]
+            entry['pixels'][i] = np.full(3, pixel)
+        # Shaping the pixels to a 224×224×3 numpy array
+        entry['pixels'] = np.array(entry['pixels'])
+        entry['pixels'] = np.reshape(entry['pixels'],(224,224,3))
+        # Converting letters to numbers from 0-26
+        entry['label'] = labels[entry['label']]
+    print("Data pre-processed.")
+    return data
+
+
+def create_data(data):
+    """Gets the data and split it into training and test sets"""
+    print("Creating the training and test sets...")
+
+    data_pixels = []
+    data_labels = []
+    for entry in data:
+        data_pixels.append(entry['pixels'])
+        data_labels.append(entry['label'])
+
+    # Splitting the data
+    offset = int(len(data)*0.7)
+    train_xs = np.array(data_pixels[:offset])
+    train_ys = np.array(data_labels[:offset])
+    test_xs = np.array(data_pixels[offset:])
+    test_ys = np.array(data_labels[offset:])
+
+    # One-Hot encoding for the labels
+    train_ys = keras.utils.to_categorical(train_ys, NUM_CLASSES)
+    test_ys = keras.utils.to_categorical(test_ys, NUM_CLASSES)
+    print("Data created.")
+    return [(train_xs, train_ys), (test_xs, test_ys)]
+
+
+def create_pickle(file_path, pickle_path):
+    """Creates pickle object of the loaded data file"""
+    data = load_data(file_path)
+    pickle_out = open(pickle_path, 'wb')
+    pickle.dump(data, pickle_out)
+    pickle_out.close()
+
+
+def load_pickle(file_path, pickle_path):
+    """Loads the pickle object"""
+    print("Loading the pickle object...")
+    pickle_in = open('data_object.pickle', 'rb')
+    data = pickle.load(pickle_in)
+    pickle_in.close()
+    print("Object Loaded.")
+    return data
+
+
 def create_model():
+    """Creates the model"""
     global model
 
-    # Creating a sequential model
     model = Sequential()
 
     # Creating the hidden layers
-    # Flattening the output of the internal activation
-    model.add(Flatten(input_shape=[7, 7, 256]))
+    # Conv Layer
+    model.add(Conv2D(input_shape=(7, 7, 1024),
+                     kernel_size=4,
+                     filters=8,
+                     strides=1,
+                     activation='relu',
+                     kernel_initializer='VarianceScaling'))
 
-    # Dense layer
-    model.add(Dense(units=100,
-                    activation='relu',
-                    kernel_initializer='varianceScaling',
-                    use_bias=True))
+    # Pooling layer to halve the output from previous layer
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    # Conv layer
+    model.add(Conv2D(kernel_size=1,
+                     filters=16,
+                     strides=1,
+                     activation='relu',
+                     kernel_initializer='VarianceScaling'))
+
+    # Pooling layer to halve the output from previous layer
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    # Flatten layer to flatten to output of the previous layer to a vector
+    model.add(Flatten())
 
     # Output layer
     model.add(Dense(units=NUM_CLASSES,
                     activation='softmax',
-                    kernel_initializer='VarianceScaling',
-                    use_bias=False))
+                    kernel_initializer='VarianceScaling'))
 
     # Compiling the model
-    adam = keras.optimizers.adam(lr=0.1)
-    model.compile(optimizer=adam,
-                  loss='categorical_crossentropy')
+    sgd = keras.optimizers.SGD(lr=0.1)
+    model.compile(optimizer=sgd,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
 
-create_data()
+def train(train_xs, train_ys, test_xs, test_ys):
+    """Trains the model"""
+    print("Training the model...")
+    model.fit(train_xs, train_ys,
+              batch_size=BATCH_SIZE,
+              validation_data=(test_xs, test_ys),
+              epochs=10000,
+              shuffle=True,
+              verbose=1)
+
+    # Checking how well the model performed
+    score = model.evaluate(test_xs, test_ys, verbose=0)
+    print('Test Loss:', score[0])
+    print('Test Accuracy: ', score[1])
+
+
+mobilenet = load_mobilenet()
+
+# Create pickle object of activations
+# print("Getting activations")
+# xs = mobilenet.predict(train_xs)
+# pickle_out = open('train_xs.pickle', 'wb')
+# pickle.dump(xs, pickle_out)
+# pickle_out.close()
+# xs = mobilenet.predict(test_xs)
+# pickle_out = open('test_xs.pickle', 'wb')
+# pickle.dump(xs, pickle_out)
+# pickle_out.close()
+
+file_path = 'data/data_100Examples.json'
+pickle_path = 'data_object.pickle'
+data = load_pickle(file_path, pickle_path)
+(train_xs, train_ys), (test_xs, test_ys) = create_data(data)
+pickle_in = open('train_xs.pickle','rb')
+train_xs = pickle.load(pickle_in)
+pickle_in.close()
+pickle_in = open('test_xs.pickle','rb')
+test_xs = pickle.load(pickle_in)
+pickle_in.close()
+
+# Creating and training the model
+create_model()
+train(train_xs,train_ys,test_xs,test_ys)
+
+# Saving the model and it's weights
+model.save_weights('model.h5')
+tfjs.converters.save_keras_model(model, 'Model')
