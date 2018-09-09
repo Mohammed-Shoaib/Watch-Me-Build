803b654cd5d23998890beb86d5ba2873eeef8e5f
Mohammed Shoaib <32389312+Mohammed-Shoaib@users.noreply.github.com>
2018-09-09 23:55:45 +0400

    Add files via upload

--- keras_model.py
+++ keras_model.py
@@ -9,15 +9,17 @@ import keras
 from keras.models import Sequential
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.backend import argmax
 from keras.applications import MobileNet
 import tensorflowjs as tfjs
 
 print("Done importing")
 
-NUM_CLASSES = 2
+NUM_CLASSES = 5
+mapping = {'A': 0, 'OKAY': 1, 'PEACE': 2, 'THUMBS UP': 3, 'Y': 4}
 BATCH_SIZE = 64
-labels = {'A': 0, 'Y': 1}
 
 
 def load_mobilenet():
@@ -37,16 +39,21 @@ def load_pickle(pickle_path):
     return data
 
 
-def load_pickle_object(file_name, pickle_path):
+def load_pickle_object(dir_name, pickle_path):
     """Loading the entire pickle object which are stored in chunks"""
-    print('Loading ' + file_name)
-    file_name += '/'
+    print('Loading ' + dir_name)
+    dir_name += '/'
     data = []
-    file_path = pickle_path + file_name
-    files = os.listdir(file_path)
-    for file in files:
-        file_path = pickle_path + file_name + file
-        data.extend(load_pickle(file_path))
+    file_path = pickle_path + dir_name
+    labels = os.listdir(file_path)
+    for label in labels:
+        label += '/'
+        label_path = pickle_path + dir_name + label
+        files = os.listdir(label_path)
+        for file in files:
+            file_path = label_path + file
+            print(file_path)
+            data.extend(load_pickle(file_path))
     data = np.array(data)
     return data
 
@@ -67,9 +74,17 @@ def show_image(image):
 def get_label(y):
     """Takes in a numpy array with one-hot encoding and output the label"""
     output_label = y.argmax(axis=-1)
-    for pose, label in  labels.items():
+    for pose, label in  mapping.items():
             if label == output_label:
                 return pose
+
+
+def shuffle_in_unison(a, b):
+    """Takes in 2 numpy arrays and shuffles them together"""
+    rng_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(rng_state)
+    np.random.shuffle(b)
 
 
 def create_model():
@@ -77,24 +92,38 @@ def create_model():
     model = Sequential()
 
     # Creating the hidden layers
-    # Flattening the output of the internal activation
-    model.add(Flatten(input_shape=[7, 7, 256]))
+    # Conv Layer
+    model.add(Conv2D(input_shape=(7, 7, 256),
+                     kernel_size=4,
+                     filters=8,
+                     strides=1,
+                     activation='relu',
+                     kernel_initializer='VarianceScaling'))
 
-    # Dense layer
-    model.add(Dense(units=100,
-                    activation='relu',
-                    kernel_initializer='VarianceScaling',
-                    use_bias=True))
+    # Pooling layer to halve the output from previous layer
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    # Conv layer
+    model.add(Conv2D(kernel_size=1,
+                     filters=16,
+                     strides=1,
+                     activation='relu',
+                     kernel_initializer='VarianceScaling'))
+
+    # Pooling layer to halve the output from previous layer
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    # Flatten layer to flatten to output of the previous layer to a vector
+    model.add(Flatten())
 
     # Output layer
     model.add(Dense(units=NUM_CLASSES,
                     activation='softmax',
-                    kernel_initializer='VarianceScaling',
-                    use_bias=False))
+                    kernel_initializer='VarianceScaling'))
 
     # Compiling the model
-    adam = keras.optimizers.adam(lr=0.01)
-    model.compile(optimizer=adam,
+    sgd = keras.optimizers.SGD(lr=0.01)
+    model.compile(optimizer=sgd,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
@@ -106,7 +135,7 @@ def train(train_xs, train_ys, test_xs, test_ys):
     model.fit(train_xs, train_ys,
               batch_size=BATCH_SIZE,
               validation_data=(test_xs, test_ys),
-              epochs=10,
+              epochs=30,
               shuffle=True,
               verbose=1)
     print("Finished training the model.")
@@ -137,28 +166,31 @@ def predict_random(test_xs, test_ys, image=False):
 
 
 mobilenet = load_mobilenet()
-pickle_path = 'data/Pickle Objects/'
 
-file_name = 'test_xs'
-test_xs = load_pickle_object(file_name, pickle_path)
-file_name = 'train_activations'
-train_activations = load_pickle_object(file_name, pickle_path)
-file_name = 'test_activations'
-test_activations = load_pickle_object(file_name, pickle_path)
-file_name = 'train_ys'
-train_ys = load_pickle_object(file_name, pickle_path)
-file_name = 'test_ys'
-test_ys = load_pickle_object(file_name, pickle_path)
+
+# Loading the data
+pickle_path = 'data/Pickle Objects/'
+# dir_name = 'test_xs'
+# test_xs = load_pickle_object(dir_name, pickle_path)
+dir_name = 'train_activations'
+train_activations = load_pickle_object(dir_name, pickle_path)
+dir_name = 'test_activations'
+test_activations = load_pickle_object(dir_name, pickle_path)
+dir_name = 'train_ys'
+train_ys = load_pickle_object(dir_name, pickle_path)
+dir_name = 'test_ys'
+test_ys = load_pickle_object(dir_name, pickle_path)
 train_ys = train_ys.astype(int)
 test_ys = test_ys.astype(int)
+shuffle_in_unison(train_activations, train_ys)
+shuffle_in_unison(test_activations, test_ys)
+
 
 # Creating and training the model
 model = create_model()
-# train(train_activations, train_ys, test_activations, test_ys)
-model.load_weights('../Model/model.h5')
+train(train_activations, train_ys, test_activations, test_ys)
 evaluate(test_activations, test_ys)
-for i in range(10):
-    predict_random(test_xs, test_ys, image=True)
+
 
 # Saving the model and it's weights
 tfjs.converters.save_keras_model(model, 'Model')
