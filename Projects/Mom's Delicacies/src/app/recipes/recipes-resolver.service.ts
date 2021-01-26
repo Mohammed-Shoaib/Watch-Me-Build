a44dd1ea0b9342aa34f7a640058974716b709544
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-01-26 06:23:10 +0100

    [TS] Initial commit for recipes resolver service

---
+++ Projects/Mom's Delicacies/src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+
+import { Recipe } from './recipe.model';
+import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
+  
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const recipes = this.recipeService.getRecipes();
+    
+    if (recipes.length === 0)
+      return this.dataStorageService.fetchRecipes();
+    else
+      return recipes;
+  }
+}
