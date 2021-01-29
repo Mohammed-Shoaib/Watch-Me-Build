125f0cc894d3ed7c4a00a629cb752f0d032015d5
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-01-29 08:55:16 +0100

    [TS] Initial commit for recipes component

---
+++ Projects/Mom's Delicacies/src/app/recipes/recipes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+}
