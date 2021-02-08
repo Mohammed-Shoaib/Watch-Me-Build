d63fa5f4c7cf8ce1cdc08775a9244b7ca9ef7d4f
Mohammed-Shoaib <shoaib98libra@gmail.com>
2021-02-08 07:08:20 +0100

    [TS] Initial commit for auth guard

---
+++ Projects/Mom's Delicacies/src/app/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+import { AuthService } from "./auth.service";
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot)
+  : boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
+    return this.authService.user
+    .pipe(
+      take(1),
+      map(user => {
+        const isAuth = !!user;
+        if (isAuth)
+          return true;
+        else
+          return this.router.createUrlTree(['/auth']);
+      })
+    );
+  }
+  
+}
